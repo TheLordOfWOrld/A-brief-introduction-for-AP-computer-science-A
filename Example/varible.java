ece5c91798844fdcdf6e1a106cd0cef89395ec77
TheLordOfWOrld <wcx1615471430@proton.me>
2024-12-22 00:49:24 +0800

    Add files via upload

---
+++ Example/varible.java
@@ -0,0 +1,11 @@
+public class varible {
+
+    double val = 0.0;
+    float val2 = 0.0f;
+    boolean val3 = false;
+    char val4 = 'a';
+    String val5 = new String("a");
+    byte val6 = 127;
+    short val7 = 32767;
+    long val8 = 9223372036854775807L;
+}
